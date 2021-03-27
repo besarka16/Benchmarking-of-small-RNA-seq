01a29823a2128dfbfaa01e19c9531b1ad76dd7e3
Šárka Benešová <36890532+besarka16@users.noreply.github.com>
2021-03-27 23:07:28 +0100

    Delete bash_check_lenght.sh

--- scripts/bash_check_lenght.sh
+++
@@ -1,9 +0,0 @@
-
-#counts lines longer than 28
-
-grep -A1 "^[@]" Qiag__MirXplore_1_SE.fastq.trim | grep -v "^[@]" | sed -n '2~2!p' | grep '.\{29\}' | wc -l
-
-#counts lines shorter than 16
-
-grep -A1 "^[@]" Qiag__MirXplore_1_SE.fastq.trim | grep -v "^[@]" | sed -n '2~2!p' | grep -E '^.{1,15}$' | wc -l
-
